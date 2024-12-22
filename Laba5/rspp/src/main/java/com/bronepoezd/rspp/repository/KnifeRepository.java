3aaf281e98c991a444549ddd561d6e9c45566f25
Mako <96295120+bronepoezd410@users.noreply.github.com>
2024-12-22 17:48:08 +0300

    Delete Laba5/rspp directory

--- Laba5/rspp/src/main/java/com/bronepoezd/rspp/repository/KnifeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bronepoezd.rspp.repository;
-
-import com.bronepoezd.rspp.model.Knife;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KnifeRepository extends JpaRepository<Knife, Long> {
-}
